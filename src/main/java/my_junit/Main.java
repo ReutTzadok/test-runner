e1bf978c048ffbcb8989d9940ad4fe523c863c3d
Reut Tzadok <reut.b1212@gmail.com>
2021-04-28 00:50:27 +0300

    final

--- src/main/java/my_junit/Main.java
+++ src/main/java/my_junit/Main.java
@@ -5,5 +5,6 @@ public class Main {
     public static void main(String[] args) {
         TestRunner testRunner = new TestRunner();
         testRunner.runAllTestsOfClass("my_junit.TestClass");
+
     }
 }
