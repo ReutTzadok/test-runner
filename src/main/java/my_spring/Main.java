05430f69fc082b1f501db6faa18d4745677bd19a
Reut Tzadok <reut.b1212@gmail.com>
2021-04-27 22:56:04 +0300

    my_spring code

---
+++ src/main/java/my_spring/Main.java
@@ -0,0 +1,9 @@
+package my_spring;
+
+public class Main {
+    public static void main(String[] args) {
+//        IRobot iRobot = ObjectFactory.getInstance().createObject(IRobot.class);
+//        IRobot iRobot2 = ObjectFactory.getInstance().createObject(IRobot.class);
+//        iRobot.cleanRoom();
+    }
+}
